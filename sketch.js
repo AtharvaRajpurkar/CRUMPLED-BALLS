5eff1b070c57186da9574380b7e01952fc117e7e
AtharvaRajpurkar <75119919+AtharvaRajpurkar@users.noreply.github.com>
2021-01-28 16:56:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1500, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	dustbin1=new dustbin(1200,600,20,100)
+	dustbin2=new dustbin(1400,600,20,100)
+	dustbin3=new dustbin(1300,650,190,20)
+	paper1=new paper(50,600,75)
+	ground=new Ground(750,680,1500,15)
+	
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+ 
+  dustbin1.display()
+  dustbin2.display() 
+  dustbin3.display()
+  paper1.display()
+  ground.display()
+
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:200,y:-200})
+	}
+}
+
+
